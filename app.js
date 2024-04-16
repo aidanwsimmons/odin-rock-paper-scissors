a8e93387bc78fc7279613b4fabceeb15d2b75ec1
Aidan Simmons <aidanwsimmons621@gmail.com>
2024-04-16 16:00:18 -0700

    updated playRound() to handle invalid inputs

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+
+//function randomly chooses from rock, paper, or scissors and returns the result as a string
 function getComputerChoice() {
-    let num = Math.floor(Math.random() * 3) + 1
+    let num = Math.floor(Math.random() * (3 - 1 + 1)) + 1
 
     if(num == 1){
         return 'rock'
@@ -12,20 +14,30 @@ function getComputerChoice() {
     }
 }
 
-function playRound(playerSelection, computerSelection){
-    computerSelection = getComputerChoice()
-    playerSelection = prompt("Rock, Paper, or Scissors?").toLowerCase()
+//function plays a round of rock paper scissors and returns a string indicating the results
+function playRound(){
+    let computerSelection = getComputerChoice()
+    let playerSelection = prompt("Enter 'rock', 'paper' or 'scissors':").toLowerCase()
+    const validChoices = ['rock', 'paper', 'scissors']
+    while(!validChoices.includes(playerSelection)){
+        playerSelection = prompt("Enter 'rock', 'paper' or 'scissors':").toLowerCase()
+    }
+    let result = ''
     //win
-    if((playerSelection = 'rock' && computerSelection == 'scissors') || (playerSelection == 'paper' && computerSelection == 'rock') || (playerSelection == 'scissors' && computerSelection == 'paper')){
-        let result = `You Win! ${playerSelection.capitalize()} beats ${computerSelection}`
+    if((playerSelection == 'rock' && computerSelection == 'scissors') || (playerSelection == 'paper' && computerSelection == 'rock') || (playerSelection == 'scissors' && computerSelection == 'paper')){
+        result = `You Win! ${playerSelection} beats ${computerSelection}`
+        console.log(playerSelection, computerSelection)
     }
     //lose
     else if(playerSelection !== computerSelection){
-        let result = `You Lose! ${computerSelection.capitalize()} beats ${playerSelection.capitalize()}`
+        result = `You Lose! ${computerSelection} beats ${playerSelection}`
+        console.log(playerSelection, computerSelection)
     }
     //tie
     else{
-        let result = "You Tie!"
+        result = "You Tie!"
+        console.log(playerSelection, computerSelection)        
     }
     return result
 }
+
